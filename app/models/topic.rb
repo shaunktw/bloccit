462ea2f6a63f900152548fe62c15c1a5ea5332bd
Shaun Koo <shaunktw@gmail.com>
2014-09-15 16:48:35 -0400

    Modified the new.html.erb page

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,7 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
 
+  scope :public, -> { where(public: true) }
+  scope :private, -> { where(public: false) }
   scope :visible_to, ->(user) {user ? all : where(public: true)}
 end
