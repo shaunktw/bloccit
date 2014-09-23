edc5785933b4d98ccdd986706152c7b5c4071960
Shaun Koo <shaunktw@gmail.com>
2014-09-22 22:29:00 -0400

    Added responsive design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,31 +10,18 @@ class ApplicationController < ActionController::Base
     redirect_to root_url, alert: exception.message
   end
 
-
- def check_for_mobile
-    session[:mobile_override] = params[:mobile] if params[:mobile]
-    prepare_for_mobile if mobile_device?
-  end
-
-  def prepare_for_mobile
-    prepend_view_path Rails.root + 'app' + 'views_mobile'
-  end
-
-  def mobile_device?
-    if session[:mobile_override]
-      session[:mobile_override] == "1"
-    else
-      # Season this regexp to taste. I prefer to treat iPad as non-mobile.
-      (request.user_agent =~ /Mobile|webOS/) && (request.user_agent !~ /iPad/)
-    end
-  end
-  helper_method :mobile_device?
-
-
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
   end
+
+
+  private
+
+  def mobile_device?
+    request.user_agent =~ /Mobile|webOS/
+  end
+  helper_method :mobile_device?
 
 end
