03282bf5f0a423c3cdb7b0e4ebc535694e696e90
Shaun Koo <shaunktw@gmail.com>
2014-09-25 23:11:27 -0400

    Updated controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
     if params[:tag]
       @posts = Post.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 10)
     else
-      @posts = Post.visible_to(current_user).where("posts.created_at > ?", 7.days.ago).paginate(page: params[:page], per_page: 10)
+      @posts = Post.visible_to(current_user).where("posts.created_at > ?", 10.days.ago).paginate(page: params[:page], per_page: 10)
     end
   end
 end
