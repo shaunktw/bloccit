4d16b67cb0f111d52933faf5b0803b497eb43081
3lviend <henryss_10@yahoo.com>
2015-02-21 01:13:35 +0700

    worked on the search post by location, city or postal code from the home pag

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,22 @@
 class PostsController < ApplicationController
   
   def index
-  	if params[:tag]
-      @posts = Post.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 10)
-    else
+  	if params[:geolocation].blank?
+  	  # if params[:tag]
+     #    @posts = Post.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 10)
+     #  else
       @posts = Post.visible_to(current_user).where("posts.created_at > ?", 10.days.ago).paginate(page: params[:page], per_page: 10)
-    end
+      # end
+    else
+      geo = Geocoder.search(params[:geolocation]).first
+      @posts = if geo.blank?
+        flash[:error] = "Location are not valid"
+        []
+      else
+        loc = Location.where(place_id: geo.data["place_id"]).first
+        @posts = loc.blank? ? [] : loc.posts.paginate(page: params[:page], per_page: 20)
+      end
+    end 
   end
 end
 
